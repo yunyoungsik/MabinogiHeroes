e1fe47b6012774aea3a0b5c643db716bb50eebd3
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-03-05 19:59:04 +0900

    20240305

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,22 +1,13 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { nav } from '@/constants';
 import Link from 'next/link';
 
 const Nav = () => {
-  const [currentPathname, setCurrentPathname] = useState(undefined);
-  const pathname = window.location.pathname;
-
-  useEffect(() => {
-    setCurrentPathname(pathname);
-  }, [pathname]);
-
   return (
     <nav>
       <ul className="w-full h-[66px] flex items-center justify-center text-basicBlack font-bold">
         {nav.map((el, index) => (
-          <li className={el.href === currentPathname ? 'nav-active h-full flex items-center justify-center mr-2' : 'h-full flex items-center justify-center mr-2'} key={index}>
+          <li className='nav-active h-full flex items-center justify-center mr-2' key={index}>
             <Link href={el.href} className='w-full text-center'>{el.name}</Link>
           </li>
         ))}
