cba0389d54ebedc847425f2f93b63e2ee52f0ddc
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-03-20 14:08:19 +0900

    20240320

--- src/app/characters/[name]/page.js
+++ src/app/characters/[name]/page.js
@@ -1,5 +1,10 @@
 import Character from "@/components/Characters/Character";
 
+export const metadata = {
+  title: '마비노기 영웅전 캐릭터 검색 - 망전',
+  description: '마비노기 영웅전 캐릭터를 검색할 수 있습니다.',
+};
+
 export default function page({params}) {
   const {name} = params;
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,63 @@ const notoSans = Noto_Sans_KR({
 });
 
 export const metadata = {
-  title: '캐릭터 검색 - 마비노기 영웅전',
-  description: '마비노기 영웅전 캐릭터 검색 사이트',
+  generator: 'Next.js',
+  title: '마비노기 영웅전 검색 사이트 - MANGJEUN',
+  description: '마비노기 영웅전 검색 사이트',
+  keywords: ['마비노기영웅전, 마영전'],
+  authors: [{ name: 'Yun' }],
+  creator: [{ name: 'Yun' }],
+  publisher: [{ name: 'Yun' }],
+  formatDetection: {
+    email: 'mangjeun@outlook.kr',
+  },
+  icons: {
+    icon: 'favicon.svg',
+  },
+  metadataBase: new URL('https://mangjeun.kro.kr'),
+  images:'https://www.mangjeun.kro.kr/image/meta/mangjeun.jpg',
+  openGraph: {
+    title: '마비노기 영웅전 검색 사이트 - MANGJEUN',
+    description:
+      '마비노기 영웅전 검색 사이트',
+    url: 'https://mangjeun.kro.kr',
+    siteName: 'MangJeun',
+    images:'https://www.mangjeun.kro.kr/image/meta/mangjeun.jpg',
+    locale: 'ko_KR',
+    type: 'website',
+    type: 'article',
+    publishedTime: '2024-03-31T00:00:00.000Z',
+    authors: ['Yun'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    Yeti: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '마비노기 영웅전 검색 사이트 - MANGJEUN',
+    description: '마비노기 영웅전 검색 사이트 - MANGJEUN',
+    images: [
+      'https://www.mangjeun.kro.kr/image/meta/mangjeun.jpg',
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
